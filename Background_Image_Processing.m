7e0e462e6c77468a0dc2120337c5b4f764e95dfc
Zijun Gao <zijung@umich.edu>
2021-09-07 12:39:36 -0400

    updated the metadata function and background image processing

---
+++ Background_Image_Processing.m
@@ -0,0 +1,127 @@
+%%% Save the first frame (ususally within the pre-release stage) of a XCA 
+%%% video as the background of synthetic XCA image. Check the quality of
+%%% the first frame, only leave those good ones for XCA synthesis
+%%% Author: Zijun Gao
+%%% Last Update: Sep 7th, 2021
+%%% Project: SyntheticAngio
+
+%-% 1. Configure the Path
+Config_Path
+Rerun_Flag = 0; 
+
+%-% 2. Iteratively save all the first frame and metadata infor based on the
+%      fields of interest
+[dicom_path_map, num_dicom_files, file_struct] = fileInfor(base_dicom_path,'.dcm');
+field_of_interest = {'PositionerPrimaryAngle','PositionerSecondaryAngle',...
+      'DistanceSourceToDetector','DistanceSourceToPatient',...
+      'EstimatedRadiographicMagnificationFactor','ExposureTime',...
+      'TableHeight','DistanceObjectToTableTop','BeamAngle'};
+
+if Rerun_Flag
+    for iFile = 1:num_dicom_files
+        dicom_file_name = file_struct(iFile).name;
+        string_splits = string(split(dicom_file_name,'.'));
+        png_save_path = fullfile(base_data_path, 'BackGround_Image','All',...
+            strcat(string_splits(1), '.png'));
+        meta_save_path = fullfile(base_data_path, 'Meta_Data',...
+            strcat(string_splits(1), '.mat'));
+        % Load the metadata
+        meta_data = angioMetadata(dicom_path_map(dicom_file_name), field_of_interest);
+        % Load the background frame, which is the first frame of the video
+        angio_video = squeeze(dicomread(dicom_path_map(dicom_file_name))); 
+        background_frame = squeeze(im2double(angio_video(:,:,1)));
+        % Save all the infor
+        imwrite(background_frame, png_save_path);
+        save(meta_save_path, 'meta_data');
+    end
+end
+
+%-% 3. Initiate a manual process to check if the quality of the first frame is Okay
+if ~isfile(fullfile(base_data_path, 'BackGround_Image', 'quality_label.csv'))
+    quality_label = zeros(1,num_dicom_files);
+    file_name = cell(num_dicom_files,1);   
+    for iFile = 1:num_dicom_files
+        dicom_file_name = file_struct(iFile).name;
+        string_splits = string(split(dicom_file_name,'.'));
+        file_name{iFile} = string_splits(1);
+        png_save_path = fullfile(base_data_path, 'BackGround_Image','All',...
+            strcat(string_splits(1), '.png'));
+        background_frame = imread(png_save_path);
+        figure;imshow(im2double(background_frame));
+        prompt = strcat(['Frame: ', num2str(iFile), 'with name', ...
+            convertStringsToChars(string_splits(1)), ', quality: ']);
+        str = input(prompt,'s');
+        if ~isempty(str)
+            if strcmp(str, 'a')
+                % some other thing there, discarded
+                quality_label(iFile) = 2;
+            elseif strcmp(str, 'd')  
+                % Need the nexy frame
+                quality_label(iFile) = 3;
+            elseif strcmp(str, 'w')
+                % Nothing to identify
+                quality_label(iFile) = 4;
+            end
+        end
+        close
+    end
+
+    index = 1:num_dicom_files;
+    quality_tab = [array2table([index', quality_label']),cell2table(file_name)];
+    writetable(quality_tab,fullfile(base_data_path, 'BackGround_Image', 'quality_label.csv'),'Delimiter',',');
+else
+    quality_tab = readtable(fullfile(base_data_path, 'BackGround_Image', 'quality_label.csv'));
+end
+
+%-% 4. Move those clean files to another folder
+good_quality = quality_tab(quality_tab.quality_label==0,:);
+if Rerun_Flag    
+    for iFile = 1:size(good_quality,1)
+        png_save_path = fullfile(base_data_path, 'BackGround_Image', 'All',...
+            strcat(good_quality.file_name(iFile), '.png'));
+        copyfile(png_save_path,fullfile(base_data_path, 'BackGround_Image', 'Clean'))
+    end
+end
+
+%-% 5. Annotate the postion of the end point of catheter with photoshop.
+% Save at Z:\Projects\Angiogram\Data\Processed\Zijun\Synthetic\BackGround_Image\Annotated
+annotated_image_dir = fullfile(base_data_path, 'BackGround_Image', 'Annotated');
+
+%-% 6. Save the position of catheter endpoint in the form of center-size
+%      Coordinates (center_x, center_y, width, height) on a (0,1) scale.
+
+for iGood = 1:size(good_quality,1)
+    %-% Read in image 
+    png_file_name = strcat(good_quality.file_name{iGood},'.png.png');
+    loaded_annotated = im2double(imread(fullfile(annotated_image_dir, png_file_name)));
+    loaded_annotated = rgb2gray(loaded_annotated);
+    annotation_binary = (loaded_annotated==1);
+    annotation = bwareafilt(annotation_binary,1);
+    %-% Obtain the location of bounding box
+    annotation_boundingbox = regionprops(annotation, 'BoundingBox').BoundingBox;
+    x_center = annotation_boundingbox(1)+annotation_boundingbox(3)/2;
+    y_center = annotation_boundingbox(2)+annotation_boundingbox(4)/2;
+    width = annotation_boundingbox(3)+2;
+    height = annotation_boundingbox(4)+2;
+    
+    %-% Readin Metadata to form the final metadata table
+    
+end
+% Information of interest in MetaDara
+
+% % Source-Intensifier Distance (SID)
+% meta_data.DistanceSourceToDetector
+% % Source-Object Distance (SOD)
+% meta_data.DistanceSourceToPatient
+% % Exposure Related
+% meta_data.EstimatedRadiographicMagnificationFactor
+% meta_data.ExposureTime
+% % Angle Related
+% meta_data.PositionerPrimaryAngle
+% meta_data.PositionerSecondaryAngle
+
+% DistanceSourceToPatient = DistanceSourceToIsocenter - 
+%     (TableHeight-DistanceObjectToTableTop)*cos(BeamAngle)
+
+%-% TODO: Get the next frame for some of the image
+% need_next_frame = quality_tab(quality_tab.quality_label==3,:);
--- util/angioMetadata.m
+++ util/angioMetadata.m
@@ -1,12 +1,15 @@
-function metadata = angioMetadata(dicom_path)
-
+function meta_data_out = angioMetadata(dicom_path, field_of_interest)
+    %%% Given the dicom path, return a metadata struct that contain fields or
+    %%% interest, if a field does not exist, mark as -1.
+    %%% Author: Zijun Gao
+    %%% Last Update: Sep 7th, 2021
+    %%% Project: SyntheticAngio
+    
     % Load metadata from the dicom file
-    metadata = load_metadata(dicom_path);
-
+    meta_data = load_metadata(dicom_path);
     % Validate the values given for primary and secondary angulations. 
-    metadata = validate_angulations(metadata);
+    meta_data_out = validate_fields(meta_data, field_of_interest);
 end
-
 
 function metadata = load_metadata(dicom_path)
     if ~isempty(dicom_path)
@@ -17,24 +20,16 @@ function metadata = load_metadata(dicom_path)
     end
 end
 
-
-function metadata = validate_angulations(metadata)
-% Set defaults for the LAO/RAO and CRA/CAU angulation if they
-% are empty in the metadata structure.
-    if isfield(metadata, 'PositionerPrimaryAngle')
-        if isempty(metadata.PositionerPrimaryAngle)
-            metadata.PositionerPrimaryAngle = 0;
+function meta_data_out = validate_fields(meta_data, field_of_interest)
+% Set value for field of interest with a default of -1.
+    meta_data_out = struct();
+    for field_cell = field_of_interest
+        % Iterate through field of interest
+        target_field = string(field_cell);
+        if isfield(meta_data,target_field)
+            meta_data_out.(target_field) = meta_data.(target_field);
+        else
+            meta_data_out.(target_field) = -1;
         end
-    else
-        warning('No LAO/RAO angulation loaded.')
-        metadata.PositionerPrimaryAngle = 0;
-    end
-    if isfield(metadata, 'PositionerSecondaryAngle')
-        if isempty(metadata.PositionerSecondaryAngle)
-            metadata.PositionerSecondaryAngle = 0;
-        end
-    else
-        warning('No CRA/CAU angulation loaded.')
-        metadata.PositionerSecondaryAngle = 0;
     end
 end
