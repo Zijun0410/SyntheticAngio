8174990e7ff4d8dc4628f45376b5267c670bd7f2
Zijun Gao <zijung@umich.edu>
2021-10-10 01:58:27 -0400

    update the GUI

--- GUI/Readme.md
+++ GUI/Readme.md
@@ -2,5 +2,8 @@ python -m PyQt5.uic.pyuic -x mainWindow.ui -o mainWindow.py
 
 https://www.py4u.net/discuss/195002 might be helpful for checking the point
 
+Install the following dependencies for dicom file processing
+```bash
 pip install python-gdcm
 pip install pydicom
+```
--- GUI/main.py
+++ GUI/main.py
@@ -2,64 +2,65 @@
 # @author: Zijun
 
 # Builtin packages and PyQt
-from PyQt5 import QtWidgets,QtCore,QtGui
+from PyQt5 import QtWidgets, QtCore, QtGui
 import os, sys
 from pathlib import Path
 import pandas as pd
+import numpy as np
 import glob
 import pydicom as dicom
 from mainWindow import Ui_MainWindow as AnnotationMainWindow 
 
 
-# class GraphicsScene(QGraphicsScene):
-#     def __init__(self, parent=None):
-#         QGraphicsScene.__init__(self, parent)
-#         self.setSceneRect(-100, -100, 200, 200)
-#         self.opt = ""
+class GraphicsScene(QtWidgets.QGraphicsScene):
+    def __init__(self, parent=None):
+        QtWidgets.QGraphicsScene.__init__(self, parent)
+        # https://doc.qt.io/qtforpython-5/PySide2/QtWidgets/QGraphicsScene.html?
+        self.index = 0
+        self.circle_items = []
 
-#     def setOption(self, opt):
-#         self.opt = opt
+    def set_index(self, num):
+        self.index = num
 
-#     def mousePressEvent(self, event):
-#         pen = QPen(QtCore.Qt.black)
-#         brush = QBrush(QtCore.Qt.black)
-#         x = event.scenePos().x()
-#         y = event.scenePos().y()
-#         if self.opt == "Generate":
-#             self.addEllipse(x, y, 4, 4, pen, brush)
-#         elif self.opt == "Select":
-#             print(x, y)
-
-
-# class SimpleWindow(QtWidgets.QMainWindow, points.Ui_Dialog):
-#     def __init__(self, parent=None):
-#         super(SimpleWindow, self).__init__(parent)
-#         self.setupUi(self)
-
-#         self.scene = GraphicsScene(self)
-#         self.graphicsView.setScene(self.scene)
-#         self.graphicsView.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
-
-#         group = QButtonGroup(self)
-#         group.addButton(self.radioButton)
-#         group.addButton(self.radioButton_2)
-
-#         group.buttonClicked.connect(lambda btn: self.scene.setOption(btn.text()))
-#         self.radioButton.setChecked(True)
-#         self.scene.setOption(self.radioButton.text())
+    def mousePressEvent(self, event):
+        # https://stackoverflow.com/questions/13965505/how-to-save-a-graphics-image-from-qgraphicsscene-in-pyqt
+        pen = QtGui.QPen(QtCore.Qt.black)
+        brush = QtGui.QBrush(QtCore.Qt.black)
+        pensize = 10
+        x = event.scenePos().x()
+        y = event.scenePos().y()
+        self.circle_items.append(self.addEllipse(x-pensize/2, y-pensize/2, pensize, pensize, pen, brush))
+        print(x, y)
 
 class MainWindow(QtWidgets.QMainWindow, AnnotationMainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
+        self.ref_size = 512
+        self.gray_color_table = [QtGui.qRgb(i, i, i) for i in range(256)]
+        self.setup_view_scene()
+
         ## Browse
         self.browseButton.clicked.connect(self.browsefoler)
         self.clearButton.clicked.connect(lambda x: self.loadDirlineEdit.setText(' '))
         self.loadDirlineEdit.textChanged.connect(lambda text: self.loadDirlineEdit.setText(str(text)))
         self.loadButton.clicked.connect(self.get_dicom_list)
-        self.indexSpinBox.valueChanged.connect(lambda num: self.display_dicom)
+        self.indexSpinBox.valueChanged.connect(lambda num: self.display_dicom(num))
         self.displayButton.clicked.connect(self.display_dicom)
+
+        self.frameHorizontalScrollBar.valueChanged.connect(lambda num: self.display_image(num-1))
+        self.previousButton.clicked.connect(self.spinbox_increase)
+        self.nextButton.clicked.connect(self.spinbox_decrease)
+        # (lambda num: self.indexSpinBox.setValue(num))
+
+    def spinbox_increase(self):
+        current_value = self.indexSpinBox.value() + 1
+        self.indexSpinBox.setValue(current_value)
+
+    def spinbox_decrease(self):
+        current_value = self.indexSpinBox.value() - 1
+        self.indexSpinBox.setValue(current_value)
 
     def browsefoler(self):      
         if len(self.loadDirlineEdit.text()) != 0 and os.path.exists(self.loadDirlineEdit.text()):
@@ -77,28 +78,43 @@ class MainWindow(QtWidgets.QMainWindow, AnnotationMainWindow):
         self.indexSpinBox.setValue(1)
         self.totalLabel.setText(str(len(self.videoPath)))
 
-    def display_dicom(self):
+    def display_dicom(self, display_index):
         # The key of dispaly: SpinBox value!!
-        display_index = self.indexSpinBox.value()
+        # display_index = self.indexSpinBox.value()
         dir_name = Path(self.videoPath[display_index]).name
         self.filenameLabel.setText(dir_name.split('.')[0])
-        ds = dicom.dcmread(self.videoPath[display_index])
-        self.ds_array = ds.pixel_array
-        count, _, _ = ds_array.shape
-        self.maxFrameLabel.setText(str(count))
-        self.frameHorizontalScrollBar.setRange(1,count)
+        dicom_read = dicom.dcmread(self.videoPath[display_index])   
+        frame_array = dicom_read.pixel_array
+        if 2**8 < np.amax(frame_array) < 2**16:
+            ratio = np.amax(frame_array) / 256     
+            self.frame_array = (frame_array / ratio).astype('uint8')
+        else:
+            self.frame_array = frame_array.astype('uint8')
+        frame_count, _, _ = self.frame_array.shape
+        self.maxFrameLabel.setText(str(frame_count))
+        self.frameHorizontalScrollBar.setRange(1,frame_count)
+        self.display_image(0)
 
-    def display_image(self):
-        self.frameGraphicsView
-        # Initate a QGraphicsScene in the Widget
-        angioScene = QtWidgets.QGraphicsScene(self)
+    def setup_view_scene(self):
+        # Initate a customized QGraphicsScene in the Widget
+        angioScene = GraphicsScene(self)
         # Initate a QGraphicsPixmapItem 
-        angioImageItem = QtWidgets.QGraphicsPixmapItem()
+        self.angioImageItem = QtWidgets.QGraphicsPixmapItem()
         # Add the item to the QGraphicsPixmapItem in the QGraphicsScene
-        angioScene.addItem(angioImageItem) 
+        angioScene.addItem(self.angioImageItem) 
         # Set the GraphicsView with the QGraphicsScene and centered on QGraphicsPixmapItem
         self.frameGraphicsView.setScene(angioScene)
-        self.frameGraphicsView.centerOn(angioImageItem)
+        self.frameGraphicsView.centerOn(self.angioImageItem)
+
+    def display_image(self, index):
+        # Create QtGui.Pixmap from self.frame_array and index for displaying
+        im = self.frame_array[index] # Shape of (n,n), n=512 or n=1024
+        frameQImage = QtGui.QImage(im.data, im.shape[1], im.shape[0], 
+            im.strides[0], QtGui.QImage.Format_Indexed8)
+        frameQImage.setColorTable(self.gray_color_table)
+        frameQPixmap = QtGui.QPixmap.fromImage(frameQImage).scaled(self.ref_size, 
+            self.ref_size, QtCore.Qt.KeepAspectRatio)
+        self.angioImageItem.setPixmap(frameQPixmap)
 
 
 if __name__ == '__main__':
